7ed0f6dd9b24d8851e9778d545bbb2dc7c916791
Sebastien Hernoux <seb_her@hotmail.fr>
2020-05-21 20:16:53 +0200

    Init repo

---
+++ README.md
@@ -0,0 +1,27 @@
+# Exposition des projets Gobelins
+
+
+## Check out our article for a step by step guide to getting this project up and running
+
+> [Prismic project guide](https://user-guides.prismic.io/examples/nuxt-js-samples/nuxtjs-sample-multi-page-website-with-navigation)
+
+
+## Launch this project in your local environment
+
+Run the following commands:
+
+``` bash
+npm install
+npm run dev
+```
+Then you can access it at [http://localhost:3000](http://localhost:3000).
+
+## Compile and minify for production and launch the server
+``` bash
+npm run build
+npm start
+```
+
+## Learn more about using Prismic with Nuxt.js
+
+> [Prismic Nuxt.js Documentation](https://prismic.io/docs/vuejs/beyond-the-api/primsic-nuxt)
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <h1>{{ $prismic.asText(title) }}</h1>
+    <p>{{ $prismic.asText(description) }}</p>
+
+    <!-- Banner component -->
+<!--    <homepage-banner :banner="banner"/>-->
+    <!-- Slices block component -->
+<!--    <slices-block :slices="slices"/>-->
+  </section>
+</template>
+
+<script>
+// Imports for all components
+import HomepageBanner from '~/components/HomepageBanner.vue'
+import SlicesBlock from '~/components/SlicesBlock.vue'
+import TextSlice from '~/components/slices/TextSlice.vue'
+
+export default {
+  name: 'Home',
+  layout: 'homepage',
+  components: {
+    HomepageBanner,
+    SlicesBlock,
+    TextSlice,
+  },
+  head () {
+    return {
+      title: 'Prismic Nuxt.js Multi Page Website',
+    }
+  },
+  async asyncData({ $prismic, error }) {
+    try{
+      // Query to get the home page content
+      const { title, description } = (await $prismic.api.getSingle('homepage')).data;
+
+      return {
+        title,
+        description,
+        // Page content
+        // banner: homepage.homepage_banner[0],
+        // Set slices as variable
+        // slices: homepage.page_content
+      }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  },
+}
+</script>
