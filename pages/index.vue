cb4b9a6a0eb0bdca2dc701ff4bcf81934d855bf9
Sebastien Hernoux <seb_her@hotmail.fr>
2020-05-22 11:57:53 +0200

    Add metadata homepage

--- pages/index.vue
+++ pages/index.vue
@@ -24,19 +24,63 @@ export default {
     SlicesBlock,
     TextSlice,
   },
-  head () {
+  head() {
+    const seo = this.tab.find(t => t.slice_type === 'seo');
+    const keywords = seo.items.map(item => item.seo_keywords);
+    if (!seo) return {};
     return {
-      title: 'Prismic Nuxt.js Multi Page Website',
-    }
+      title: seo.primary.seo_title,
+      meta: [
+        { hid: 'description',
+          name: 'description',
+          content: seo.primary.seo_description },
+        { hid: 'og:description',
+          name: 'og:description',
+          content: seo.primary.seo_description },
+        { hid: 'keywords',
+          name: 'keywords',
+          content: seo.items.length ? keywords.join(', ') : '' },
+        /*
+        { hid: 'og:image',
+          property: 'og:image',
+          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.url : '' },
+        { hid: 'og:image:width',
+          property: 'og:image:width',
+          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.details.image.width : '' },
+        {
+          hid: 'og:image:height',
+          property: 'og:image:height',
+          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.details.image.height : '',
+        },
+        {
+          hid: 'og:image:type',
+          property: 'og:image:type',
+          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.contentType : '',
+        },
+        {
+          hid: 'og:image:type',
+          property: 'og:image:type',
+          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.contentType : '',
+        },
+        */
+        { hid: 'og:url',
+          property: 'og:url',
+          content: process.env.HOSTNAME + this.$route.fullPath }, // Add HOSTNAME in .env for full URL
+        { hid: 'og:title',
+          property: 'og:title',
+          content: seo.primary.seo_title },
+      ],
+    };
   },
   async asyncData({ $prismic, error }) {
     try{
       // Query to get the home page content
-      const { title, description } = (await $prismic.api.getSingle('homepage')).data;
+      const { title, description, body1 } = (await $prismic.api.getSingle('homepage')).data;
 
       return {
         title,
         description,
+        tab: body1
         // Page content
         // banner: homepage.homepage_banner[0],
         // Set slices as variable
