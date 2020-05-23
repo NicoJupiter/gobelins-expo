5ff15eb1342664e0091a79fd717ac941029f9ede
Sébastien H <seb_her@hotmail.fr>
2020-05-23 14:18:28 +0200

    Delete index.js

--- components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header';
-export { default as HomepageBanner } from './HomepageBanner';
-export { default as Footer } from './Footer';
-export { default as SlicesBlock } from './SlicesBlock';
