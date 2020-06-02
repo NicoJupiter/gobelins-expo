067d4688a81c928698cf2597fcc1e8d39837b4fd
Sebastien Hernoux <seb_her@hotmail.fr>
2020-06-02 10:38:28 +0200

    Link projects on homepage

---
+++ constantes/index.js
@@ -0,0 +1,4 @@
+export const PAGES_SLUG = {
+  droneproject: '/drones',
+  webglproject: '/webgl',
+};
--- pages/index.vue
+++ pages/index.vue
@@ -9,107 +9,131 @@
     <p class="homepage__desc">{{ $prismic.asText(description) }}</p>
 
     <div class="project-container">
-      <div class="project" v-for="(p, i) in projects" :key="i">
-        <h3>{{ $prismic.asText(p.data.title) }}</h3>
-      </div>
+      <n-link
+          v-for="(p, i) in projects"
+          :key="i"
+          :to="p.slug"
+          class="project"
+      >
+        <div>
+          <h3>{{ $prismic.asText(p.data.title) }}</h3>
+        </div>
+      </n-link>
     </div>
 
     <!-- Banner component -->
-<!--    <homepage-banner :banner="banner"/>-->
+    <!--    <homepage-banner :banner="banner"/>-->
     <!-- Slices block component -->
-<!--    <slices-block :slices="slices"/>-->
+    <!--    <slices-block :slices="slices"/>-->
   </section>
 </template>
 
 <script>
-// Imports for all components
-import HomepageBanner from '~/components/HomepageBanner.vue'
-import SlicesBlock from '~/components/SlicesBlock.vue'
-import TextSlice from '~/components/slices/TextSlice.vue'
-import Scene from '~/components/Scene.vue'
+  // Imports for all components
+  import HomepageBanner from '~/components/HomepageBanner.vue'
+  import SlicesBlock from '~/components/SlicesBlock.vue'
+  import TextSlice from '~/components/slices/TextSlice.vue'
+  import Scene from '~/components/Scene.vue'
+  import { PAGES_SLUG } from "../constantes";
 
-export default {
-  name: 'Home',
-  layout: 'homepage',
-  components: {
-    HomepageBanner,
-    SlicesBlock,
-    TextSlice,
-    Scene,
-  },
-  head() {
-    const seo = this.tab.find(t => t.slice_type === 'seo');
-    const keywords = seo.items.map(item => item.seo_keywords);
-    if (!seo) return {};
-    return {
-      title: seo.primary.seo_title,
-      meta: [
-        { hid: 'description',
-          name: 'description',
-          content: seo.primary.seo_description },
-        { hid: 'og:description',
-          name: 'og:description',
-          content: seo.primary.seo_description },
-        { hid: 'keywords',
-          name: 'keywords',
-          content: seo.items.length ? keywords.join(', ') : '' },
-        /*
-        { hid: 'og:image',
-          property: 'og:image',
-          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.url : '' },
-        { hid: 'og:image:width',
-          property: 'og:image:width',
-          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.details.image.width : '' },
-        {
-          hid: 'og:image:height',
-          property: 'og:image:height',
-          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.details.image.height : '',
-        },
-        {
-          hid: 'og:image:type',
-          property: 'og:image:type',
-          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.contentType : '',
-        },
-        {
-          hid: 'og:image:type',
-          property: 'og:image:type',
-          content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.contentType : '',
-        },
-        */
-        { hid: 'og:url',
-          property: 'og:url',
-          content: process.env.HOSTNAME + this.$route.fullPath }, // Add HOSTNAME in .env for full URL
-        { hid: 'og:title',
-          property: 'og:title',
-          content: seo.primary.seo_title },
-      ],
-    };
-  },
-  async asyncData({ $prismic, error }) {
-    try{
-      // Query to get the home page content
-      const { title, description, body1 } = (await $prismic.api.getSingle('homepage')).data;
-
-      const dronesDocs = (await $prismic.api.getSingle('webglpage')).data;
-      const webglDocs = (await $prismic.api.getSingle('dronepage')).data;
-      const projectsIds = [...dronesDocs.projects, ...webglDocs.projects].map(p => p.project.id);
-      const projects = (await $prismic.api.getByIDs(projectsIds)).results;
-
+  export default {
+    name: 'Home',
+    layout: 'homepage',
+    components: {
+      HomepageBanner,
+      SlicesBlock,
+      TextSlice,
+      Scene,
+    },
+    head() {
+      const seo = this.tab.find(t => t.slice_type === 'seo');
+      const keywords = seo.items.map(item => item.seo_keywords);
+      if (!seo) return {};
       return {
-        title,
-        description,
-        tab: body1,
-        projects,
-        // Page content
-        // banner: homepage.homepage_banner[0],
-        // Set slices as variable
-        // slices: homepage.page_content
+        title: seo.primary.seo_title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: seo.primary.seo_description
+          },
+          {
+            hid: 'og:description',
+            name: 'og:description',
+            content: seo.primary.seo_description
+          },
+          {
+            hid: 'keywords',
+            name: 'keywords',
+            content: seo.items.length ? keywords.join(', ') : ''
+          },
+          /*
+           { hid: 'og:image',
+           property: 'og:image',
+           content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.url : '' },
+           { hid: 'og:image:width',
+           property: 'og:image:width',
+           content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.details.image.width : '' },
+           {
+           hid: 'og:image:height',
+           property: 'og:image:height',
+           content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.details.image.height : '',
+           },
+           {
+           hid: 'og:image:type',
+           property: 'og:image:type',
+           content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.contentType : '',
+           },
+           {
+           hid: 'og:image:type',
+           property: 'og:image:type',
+           content: this.datas.fields.seo.fields.image ? this.datas.fields.seo.fields.image.fields.file.contentType : '',
+           },
+           */
+          {
+            hid: 'og:url',
+            property: 'og:url',
+            content: process.env.HOSTNAME + this.$route.fullPath
+          }, // Add HOSTNAME in .env for full URL
+          {
+            hid: 'og:title',
+            property: 'og:title',
+            content: seo.primary.seo_title
+          },
+        ],
+      };
+    },
+    async asyncData({ $prismic, error }) {
+      try {
+        // Query to get the home page content
+        const { title, description, body1 } = (await $prismic.api.getSingle('homepage')).data;
+
+        const dronesDocs = (await $prismic.api.getSingle('webglpage')).data;
+        const webglDocs = (await $prismic.api.getSingle('dronepage')).data;
+        const projectsIds = [...dronesDocs.projects, ...webglDocs.projects].map(p => p.project.id);
+        let projects = (await $prismic.api.getByIDs(projectsIds)).results;
+
+
+        projects = projects.map(p => ({
+          ...p,
+          slug: `${PAGES_SLUG[p.type]}/${p.slugs[0]}`,
+        }));
+
+        return {
+          title,
+          description,
+          tab: body1,
+          projects,
+          // Page content
+          // banner: homepage.homepage_banner[0],
+          // Set slices as variable
+          // slices: homepage.page_content
+        }
+      } catch (e) {
+        error({ statusCode: 404, message: 'Page not found' })
       }
-    } catch (e) {
-      error({ statusCode: 404, message: 'Page not found' })
-    }
-  },
-}
+    },
+  }
 </script>
 
 <style lang="sass" scoped>
